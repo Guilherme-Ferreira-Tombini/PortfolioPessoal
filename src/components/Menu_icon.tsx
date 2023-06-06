baf14432fdfa76287db19e4fb0656d51d3d3a193
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2023-06-06 11:29:49 -0300

    Ajustado menu com os icones

--- src/components/Menu_icon.tsx
+++ src/components/Menu_icon.tsx
@@ -7,16 +7,16 @@ export default function Menu_icon() {
   return (
   <>
   {/* Opções de pagina */}
-  <div className="w-[64px] h-3/6 mt-9 flex flex-col flex-wrap">
+  <div className="w-[64px] h-[55%] mt-9 flex flex-col flex-wrap">
      {/* Div para separar os icones */}
-      <div className="flex flex-col justify-between h-[80%] p-3">
-        <a href="/" className="transition duration-500 ease-in-out hover:scale-110">
+      <div className="flex flex-col items-center justify-around h-[80%]">
+        <a href="/" className="transition duration-500 ease-in-out hover:scale-110 w-10 h-10">
           <Image src={inicio} alt="Icone da pagina inicial"/>
         </a>
-        <a href="/projetos" className="transition duration-500 ease-in-out hover:scale-110">
+        <a href="/projetos" className="transition duration-500 ease-in-out hover:scale-110 w-10 h-10">
           <Image src={projetos} alt="Icone da pagina de projetos"/>
         </a>
-        <a href="/perfil" className="transition duration-500 ease-in-out hover:scale-110">
+        <a href="/perfil" className="transition duration-500 ease-in-out hover:scale-110 w-10 h-10">
           <Image src={perfil} alt="Icone da pagina perfil"/>
         </a>
      </div>
