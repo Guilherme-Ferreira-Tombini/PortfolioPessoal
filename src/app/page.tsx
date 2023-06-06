5d2a9103b56feda2b864afeff920b565dcae1e78
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2023-06-05 23:03:33 -0300

    Inicialização da estilização da pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
 import Menu from "@/components/Menu"
+import Texto from "../components/Texto_Inicial" 
+import Image from "next/image"
+
+import Nuvem_superior from "../assets/nuvem-cantoSuperior.png"
+import Nuvem_inferior from "../assets/nuvem-cantoInferior.png"
+
 export default function Home() {
   return (
    <div className="flex flex-row w-screen h-screen">
-
+    {/* Menu em componente */}
      <Menu/>
-     <div className="">
-       
+
+     {/* Meio da Pagina */}
+     <div className="w-[100%] h-[100vh] flex flex-col justify-center items-center">
+
+       {/* Nuvem de efeito da pagina - Superior */}
+       <Image className="absolute right-0 top-0" src={Nuvem_superior} alt="Nuvem superior da pagina"/>
+
+      {/* Texto referente a apresentação do portfolio  */}
+       <Texto/>
+
+      {/* Nuvem de efeito da pagina - Inferior */}
+       <Image className="absolute left-[88px] bottom-0" src={Nuvem_inferior} alt="Nuvem inferior da pagina"/> 
+
+
+        {/* Copyright */}
+      <div className="w-[100%] flex flex-col items-end p-4">
+       <p className="text-green-800">
+         Copyright © 2023 de Guilherme Tombini
+       </p>
+      </div>
+
      </div>
-
-
 
    </div>
   )
---
+++ src/components/Texto_Inicial.tsx
@@ -0,0 +1,15 @@
+export default function Texto_Inicial() {
+  return (
+  <>
+     {/* div para o texto */}
+     <div className="w-3/4 h-[93%] flex flex-col justify-center items-center">
+         <h1 className="text-6xl text-green-950"> Bem vindo!👨‍💻</h1>
+         <p className="w-[80%] text-2xl text-justify text-green-800 mt-8">
+           Olá, espero que esteja bem, sou o Guilherme e este é o meu portfolio pessoal.<br></br><br></br>
+           Aqui vou mostrar projetos, conhecimentos e habilidades que adquiri na programação desde o ano de 2020 até o ano atual, 
+           caso eu conheça mais uma tecnologia nos meus estudos, posso atualizar esse site.<br></br><br></br>Espero que gostem :)
+         </p>
+       </div>
+  </>
+  )
+}
