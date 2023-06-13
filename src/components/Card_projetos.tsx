95f73480784ff63f04fb59055c96fd2dd860601c
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2023-06-13 15:56:01 -0300

    Ajustado pagina de Projetos para ser Scroll

--- src/components/Card_projetos.tsx
+++ src/components/Card_projetos.tsx
@@ -1,10 +1,13 @@
 // Componente relacionado ao card de cada projeto do GitHub, utilizando "props"
 export default function Card_projetos(props:any) {
   return (
-   <div className="bg-black text-white m-2" key={props.id}>
+   <div className="bg-black text-white m-6 p-4 w-[38%] h-[43%] rounded-xl" key={props.id}>
       <h1>{props.name}</h1>
       <h5>Ultima edição: {props.updated_at}</h5>
-      <h5>Link Github: {props.html_url}</h5>
+      <h5>Link Github: <a className="underline" href={props.html_url}>
+                          Acessar projeto
+                       </a>
+      </h5>
       <h5>Linguagem mais utilizada: {props.language}</h5>
       <h5>Total de Linguagens:</h5>
    </div>
--- src/components/Projetos_pagina.tsx
+++ src/components/Projetos_pagina.tsx
@@ -7,11 +7,6 @@ import { useEffect, useState } from "react";
 import Card_projetos from './Card_projetos'
 
 
-// Imagens das setas
-import setaDireita from '../assets/botao-de-seta-para-a-direita-do-teclado.png'
-import setaEsquerda from '../assets/botao-esquerda.png'
-
-
 // Tipagem dos dados para a utilização das informações na linha 40
 interface Dados{
   id: number,
@@ -45,17 +40,10 @@ export default function Projetos_pagina() {
    }
 
   return (
-   <div className="flex flex-row w-[100%] h-[85%]">
+   <div className="flex flex-row w-[95%] overflow-x-hidden overflow-y h-[84%]">
      
-     {/* Seta da esquerda */}
-     <div className=" flex flex-col items-center justify-center p-9 w-[10%] h-[100%]">
-       <Image src={setaEsquerda} alt="seta esquerda"/>
-     </div>
-
-
-
      {/* Meio do carrossel */}
-     <div className="bg-green-300 flex flex-row flex-nowrap w-[80%] h-[100%]">
+     <div className="flex flex-row justify-center flex-wrap">
        {
          // Utilização dos dados dos projetos
          Api.map((dado:Dados) =>{
@@ -65,14 +53,6 @@ export default function Projetos_pagina() {
          })
        }
      </div>
-
-
-
-     {/* Seta da direita */}
-     <div className=" flex flex-col items-center justify-center p-9 w-[10%] h-[100%]">
-       <Image src={setaDireita} alt="seta direita"/>
-     </div>
-
 
    </div>
   )
