e0cd9abc6f7337bb327c23ad1d2e943dd2355eaa
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2023-06-13 12:10:20 -0300

    Corrigido local de utilização da leitura dos dados

--- src/components/Card_projetos.tsx
+++ src/components/Card_projetos.tsx
@@ -1,52 +1,13 @@
-"use client"
-import api from "@/app/api/api";
-import { useEffect, useState } from "react";
-
-// Tipagem dos dados para a utilização das informações na linha 40
-interface Dados{
-  id: number,
-  name: any,
-  html_url: any,
-  updated_at: any
-}
-
-
-export default function Card_projetos() {
-  // Variavel para armazenar os dados sobre os projetos do GitHub
- const [Api,setApi]=useState([]);
-
-// Execução da leitura da api para pegar os dados sobre os projetos
-  useEffect(()=>{
-    api.get("/users/Guilherme-Ferreira-Tombini/repos")
-      .then(response =>{
-       setApi(response.data)
-      })
-      .catch(err => {
-        console.log("Deu ruim ", err)
-      })
-  }, [] )
-
-
-  // Caso os dados ainda estejam carregando, ira mostrar a frase de carregamento
-  if (!Api) {
-    return (
-      <h1>Carregando projetos...</h1>
-    );
-  }
-
+// Componente relacionado ao card de cada projeto do GitHub, utilizando "props"
+export default function Card_projetos(props:any) {
   return (
-   <>
-   {
-     // Utilização dos dados dos projetos
-     Api.map((dado:Dados) =>{
-       return(
-         <div key={dado.id}>
-             <h1>{dado.name}</h1>
-         </div>
-       )
-     })
-   }
-   </>
+   <div className="bg-black text-white m-2" key={props.id}>
+      <h1>{props.name}</h1>
+      <h5>Ultima edição: {props.updated_at}</h5>
+      <h5>Link Github: {props.html_url}</h5>
+      <h5>Linguagem mais utilizada: {props.language}</h5>
+      <h5>Total de Linguagens:</h5>
+   </div>
   )
 }
 
--- src/components/Projetos_pagina.tsx
+++ src/components/Projetos_pagina.tsx
@@ -1,13 +1,49 @@
+"use client"
 import Image from 'next/image'
+import api from "@/app/api/api";
+import { useEffect, useState } from "react";
 
 // Importação do Componente com os cards
 import Card_projetos from './Card_projetos'
+
 
 // Imagens das setas
 import setaDireita from '../assets/botao-de-seta-para-a-direita-do-teclado.png'
 import setaEsquerda from '../assets/botao-esquerda.png'
 
+
+// Tipagem dos dados para a utilização das informações na linha 40
+interface Dados{
+  id: number,
+  name: string,
+  html_url: string,
+  updated_at: string,
+  language: string,
+}
+
+
 export default function Projetos_pagina() {
+  // Variavel para armazenar os dados sobre os projetos do GitHub
+ const [Api,setApi]=useState([]);
+
+ // Execução da leitura da api para pegar os dados sobre os projetos
+   useEffect(()=>{
+     api.get("/users/Guilherme-Ferreira-Tombini/repos")
+       .then(response =>{
+        setApi(response.data)
+       })
+       .catch(err => {
+         console.log("Deu ruim ", err)
+       })
+   }, [] )
+ 
+   // Caso os dados ainda estejam carregando, ira mostrar a frase de carregamento
+   if (!Api) {
+     return (
+       <h1>Carregando projetos...</h1>
+     );
+   }
+
   return (
    <div className="flex flex-row w-[100%] h-[85%]">
      
@@ -16,10 +52,21 @@ export default function Projetos_pagina() {
        <Image src={setaEsquerda} alt="seta esquerda"/>
      </div>
 
+
+
      {/* Meio do carrossel */}
-     <div className="bg-green-300 w-[80%] h-[100%]">
-     <Card_projetos/>
+     <div className="bg-green-300 flex flex-row flex-nowrap w-[80%] h-[100%]">
+       {
+         // Utilização dos dados dos projetos
+         Api.map((dado:Dados) =>{
+         return(
+          <Card_projetos id={dado.id} name={dado.name} html_url={dado.html_url}  updated_at={dado.updated_at} language={dado.language}/>
+         )
+         })
+       }
      </div>
+
+
 
      {/* Seta da direita */}
      <div className=" flex flex-col items-center justify-center p-9 w-[10%] h-[100%]">
