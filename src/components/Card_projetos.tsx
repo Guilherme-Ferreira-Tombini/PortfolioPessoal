197a8bd26011a164824a7763d9548ff5524d9135
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2023-06-16 09:43:04 -0300

    Adicionado condição para quando não tem a linguagem mais utilizada e colocado link para abrir em outra guia

--- src/components/Card_projetos.tsx
+++ src/components/Card_projetos.tsx
@@ -6,6 +6,7 @@ import moment from "moment"
 export default function Card_projetos(props:any) {
    // setando a data para ser do tipo brasileiro
    moment.locale('pt-br');
+   
    // armazenando em uma variavel a data da ultima atualização do projeto do GitHub
    let data = props.pushed_at;
 
@@ -13,12 +14,11 @@ export default function Card_projetos(props:any) {
    <div className="bg-green-950 text-white m-6 p-4 w-[38%] h-[43%] rounded-xl flex flex-col" >
       <h1 className="text-xl flex flex-col justify-center items-center mb-2">{props.name}</h1>
       <h5 className="">Ultima edição: {moment(data).format('DD/MM/YYYY')}</h5>
-      <h5 className="">Link Github: <a className="underline" href={props.html_url}>
+      <h5 className="">Link Github: <a className="underline" href={props.html_url} target="_blank">
                           Acessar projeto
                        </a>
       </h5>
-      <h5 className="">Linguagem mais utilizada: {props.language}</h5>
-      <h5 className="">Total de Linguagens:</h5>
+      <h5 className="">Linguagem mais utilizada: {props.language != null ? props.language: "Não existe linguagem mais utilizada neste projeto"}</h5>
    </div>
   )
 }
